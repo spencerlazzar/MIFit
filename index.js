2d3d820995f97165e96badde8b9f374d03d35c32
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-05-10 15:19:26 -0700

    Updated Social Links on the Bottom

---
+++ index.js
@@ -0,0 +1,7 @@
+var fields = {
+  document.addEventListener("DOMContentLoaded", function() {
+    fields.fullName = document.getElementById('fullName');
+    fields.email = document.getElementById('email');
+    fields.message = document.getElementById('message');
+  })
+}
